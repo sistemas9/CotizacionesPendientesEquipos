d05a5e730173d0f9f60ddead38c9a9babe33de32
sistemas9 <sistemas9@avanceytec.com.mx>
2022-05-18 15:43:05 -0600

    Commit para pdf de prueba
    Se genrea el pdf de prueba esperando validacion para que se suba periodicamente

--- Program.cs
+++ Program.cs
@@ -18,11 +18,13 @@ namespace CotizacionesPendientesEquipos
 {
   class Program
   {
+    public static EmployeesResult workers;
     static void Main(string[] args)
     {
       DateTime inicioGetList = DateTime.Now;
       Console.WriteLine("Inicio obtencion de cotizaciones: {0}",inicioGetList.ToString());
       SalesQuotationResult salesQuotationResult = GetListOfQuoations();
+      workers = GetListOfEmployees();
       DateTime finGetList = DateTime.Now;
       Console.WriteLine("Fin de obtencion de cotizaciones: {0}", finGetList.ToString());
       Console.WriteLine("Total de tiempo en obtencion: {0}",((finGetList - inicioGetList).TotalSeconds).ToString());
@@ -49,7 +51,7 @@ namespace CotizacionesPendientesEquipos
       Console.WriteLine("Total de tiempo en equipo or refaccion: {0}", ((finEqOrRef - inicioEqOrRef).TotalMinutes).ToString());
       Console.WriteLine("Total de cotizaciones: {0}", EquipoOrRefaccionSalesQuotation.Count);
       EquipoOrRefaccionSalesQuotation = EquipoOrRefaccionSalesQuotation.Distinct().ToList();
-      EquipoOrRefaccionSalesQuotation = EquipoOrRefaccionSalesQuotation.OrderBy(cot => cot.SalesQuotationNumber).ToList();
+      EquipoOrRefaccionSalesQuotation = EquipoOrRefaccionSalesQuotation.OrderBy(cot => cot.DefaultShippingSiteId).ThenBy(cot2 => cot2.SalesQuotationNumber).ToList();
       String template = GetTemplateHTML(EquipoOrRefaccionSalesQuotation);
       StringReader sr = new StringReader(template);
       using (MemoryStream ms2 = new MemoryStream())
@@ -93,10 +95,19 @@ namespace CotizacionesPendientesEquipos
     {
       SalesQuotationResult SalesQuotations = new SalesQuotationResult();
       ConsultaEntity entity = new ConsultaEntity();
-      String url = "https://ayt.operations.dynamics.com/Data/SalesQuotationHeadersV2?$filter=SalesQuotationStatus eq Microsoft.Dynamics.DataEntities.SalesQuotationStatus'Created' and ReceiptDateRequested gt 2022-05-10T12:00:00Z&$expand=SalesQuotationLines($select=ItemNumber)&$select=SalesQuotationNumber,ReceiptDateRequested,RequestingCustomerAccountNumber,SalesQuotationName,QuotationTakerPersonnelNumber";
+      String url = "https://ayt.operations.dynamics.com/Data/SalesQuotationHeadersV2?$filter=SalesQuotationStatus eq Microsoft.Dynamics.DataEntities.SalesQuotationStatus'Created' and ReceiptDateRequested gt 2022-05-10T12:00:00Z&$expand=SalesQuotationLines($select=LineCreationSequenceNumber,ItemNumber,LineDescription,SalesUnitSymbol,RequestedSalesQuantity,LineAmount)&$select=SalesQuotationNumber,ReceiptDateRequested,RequestingCustomerAccountNumber,SalesQuotationName,QuotationTakerPersonnelNumber,QuotationResponsiblePersonnelNumber";
       var result = entity.QueryEntity(url);
       SalesQuotations = JsonConvert.DeserializeObject<SalesQuotationResult>(result.Result.Content);
       return SalesQuotations;
+    }
+
+    public static EmployeesResult GetListOfEmployees()
+    {
+      ConsultaEntity entity = new ConsultaEntity();
+      String url = "https://ayt.operations.dynamics.com/Data/Employees?$select=PersonnelNumber,Name";
+      var result = entity.QueryEntity(url);
+      EmployeesResult employees = JsonConvert.DeserializeObject<EmployeesResult>(result.Result.Content);
+      return employees;
     }
 
     public static Boolean IsEquipoOrRefaccion(String ItemNumber)
@@ -115,27 +126,89 @@ namespace CotizacionesPendientesEquipos
     public static String GetTemplateHTML(List<SalesQuotation> origen)
     {
       String html = "";
-      html += "<table style=\"border-collapse:collapse; width: 90 %; font-size: 11px; margin: 20px !important; page-break-inside:avoid;\">";
-      html += "  <thead>";
-      html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
-      html += "      <td style=\"border: solid 1px black; color:white; background-color: #2874A6; width:15%;\">Cotizacion</td>";
-      html += "      <td style=\"border: solid 1px black; color:white; background-color: #2874A6; width:20%;\">Fecha</td>";
-      html += "      <td style=\"border: solid 1px black; color:white; background-color: #2874A6; width:15%;\">Cod. Cliente</td>";
-      html += "      <td style=\"border: solid 1px black; color:white; background-color: #2874A6; width:50%;\">Nombe cliente</td>";
-      html += "    </tr>";
-      html += "  </thead>";
-      html += "  <tbody>";
       foreach(SalesQuotation cotizacion in origen)
       {
-        html += "    <tr style=\"width: 90%;\">";
-        html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + cotizacion.SalesQuotationNumber + "</td>";
-        html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + Convert.ToDateTime(cotizacion.ReceiptDateRequested).ToString("yyyy-MM-dd") + "</td>";
-        html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + cotizacion.RequestingCustomerAccountNumber + "</td>";
-        html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + cotizacion.SalesQuotationName + "</td>";
+        String SalesTaker = workers.value.Where(emp => emp.PersonnelNumber.Equals(cotizacion.QuotationTakerPersonnelNumber)).ToList()[0].Name;
+        String SalesResponsible = workers.value.Where(emp => emp.PersonnelNumber.Equals(cotizacion.QuotationResponsiblePersonnelNumber)).ToList()[0].Name;
+        html += "<table style=\"border-collapse:collapse; width: 90 %; font-size: 11px; margin: 20px !important; page-break-inside:avoid; border:solid 2px #CCC;\">";
+        html += "  <thead>";
+        html += "    <tr>";
+        html += "      <td colspan=\"8\">&nbsp;</td>";
         html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th>&nbsp;</th>";
+        html += "      <th colspan=\"6\"><b>ID Cliente: </b>" + cotizacion.RequestingCustomerAccountNumber + "</th>";
+        html += "      <th>&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th>&nbsp;</th>";
+        html += "      <th colspan=\"6\"><b>Nombre Cliente: </b>" + cotizacion.SalesQuotationName + "</th>";
+        html += "      <th>&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th colspan=\"8\">&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th>&nbsp;</th>";
+        html += "      <th colspan=\"6\"><b>Cotizacion: </b>" + cotizacion.SalesQuotationNumber + "</th>";
+        html += "      <th>&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th>&nbsp;</th>";
+        html += "      <th colspan=\"6\"><b>Total: </b>{TotalCotizacion}</th>";
+        html += "      <th>&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th colspan=\"8\">&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th>&nbsp;</th>";
+        html += "      <th colspan=\"6\"><b>Sales Taker: </b>" + SalesTaker + "</th>";
+        html += "      <th>&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr style=\"font-weight:bolder;width: 90%;\">";
+        html += "      <th>&nbsp;</th>";
+        html += "      <th colspan=\"6\"><b>Sales Responsible: </b>" + SalesResponsible + "</th>";
+        html += "      <th>&nbsp;</th>";
+        html += "    </tr>";
+        html += "    <tr>";
+        html += "      <th colspan=\"8\">&nbsp;</th>";
+        html += "    </tr>";
+        html += "  </thead>";
+        html += "  <tbody>";
+        html += "    <tr style=\"width: 90%;\">";
+        html += "      <td>&nbsp;</td>";
+        html += "      <td style=\"border: solid 1px black; font-size: 11px; font-weight: bolder;\">Linea</td>";
+        html += "      <td style=\"border: solid 1px black; font-size: 11px; font-weight: bolder;\">Item</td>";
+        html += "      <td style=\"border: solid 1px black; font-size: 11px; font-weight: bolder;\">Descripcion</td>";
+        html += "      <td style=\"border: solid 1px black; font-size: 11px; font-weight: bolder;\">Unidad</td>";
+        html += "      <td style=\"border: solid 1px black; font-size: 11px; font-weight: bolder;\">Cantidad</td>";
+        html += "      <td style=\"border: solid 1px black; font-size: 11px; font-weight: bolder;\">Total</td>";
+        html += "      <td>&nbsp;</td>";
+        html += "    </tr>";
+        Double totalCoti = 0;
+        foreach (SalesQuotationLines lines in cotizacion.SalesQuotationLines)
+        {
+          html += "    <tr style=\"width: 90%;\">";
+          html += "      <td>&nbsp;</td>";
+          html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + lines.LineCreationSequenceNumber + "</td>";
+          html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + lines.ItemNumber + "</td>";
+          html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + lines.LineDescription + "</td>";
+          html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + lines.SalesUnitSymbol + "</td>";
+          html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + lines.RequestedSalesQuantity.ToString()+ "</td>";
+          html += "      <td style=\"border: solid 1px black; font-size: 10px;\">" + String.Format("{0:C}", lines.LineAmount) + "</td>";
+          html += "      <td>&nbsp;</td>";
+          html += "    </tr>";
+          totalCoti += lines.LineAmount;
+        }
+        html += "    <tr>";
+        html += "      <td colspan=\"8\">&nbsp;</td>";
+        html += "    </tr>";
+        html += "  </tbody>";
+        html += "</table>";
+        html = html.Replace("{TotalCotizacion}", String.Format("{0:C}", totalCoti));
+        totalCoti = 0;
       }
-      html += "  </tbody>";
-      html += "</table>";
       return html;
     }
   }
@@ -152,12 +225,19 @@ namespace CotizacionesPendientesEquipos
     public String RequestingCustomerAccountNumber { get; set; }
     public String SalesQuotationName { get; set; }
     public String QuotationTakerPersonnelNumber { get; set; }
+    public String QuotationResponsiblePersonnelNumber { get; set; }
+    public String DefaultShippingSiteId { get; set; }
     public List<SalesQuotationLines> SalesQuotationLines { get; set; }
   }
 
   public class SalesQuotationLines
   {
+    public Double LineCreationSequenceNumber { get; set; }
     public String ItemNumber { get; set; }
+    public String LineDescription { get; set; }
+    public String SalesUnitSymbol { get; set; }
+    public Double RequestedSalesQuantity { get; set; }
+    public Double LineAmount { get; set; }
   }
 
   public class ProductCategory
@@ -169,5 +249,16 @@ namespace CotizacionesPendientesEquipos
   public class ProductCategoryResult
   {
     public List<ProductCategory> value { get; set; }
+  }
+
+  public class Employees
+  {
+    public String PersonnelNumber { get; set; }
+    public String Name { get; set; }
+  }
+
+  public class EmployeesResult
+  {
+    public List<Employees> value { get; set; }
   }
 }
